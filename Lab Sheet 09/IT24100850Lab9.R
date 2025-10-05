f873f19ecd199736056bc8f533471651b9d17368
IT24100850 <it24100850@my.sliit.lk>
2025-10-05 21:12:43 +0530

    Add files via upload

---
+++ Lab Sheet 09/IT24100850Lab9.R
@@ -0,0 +1,8 @@
+setwd("C:\\Users\\ASUS\\Desktop\\IT24100850Lab_9")
+
+#1
+times <- c(44,47,45,46,43,45,44,48,46,47,45,46,44,45,47,46,45,44,46,47,45,46,44,45,46)
+
+
+#2
+t.test(times, mu=46, alternative = "less")
